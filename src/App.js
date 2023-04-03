37dc4150935b74131c2cf3fff041fb54310b608a
kiran Shrestha <kiranstha1679@gmail.com>
2023-04-03 16:43:40 +0545

    first push

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Learning AWS hosting with amplify
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
